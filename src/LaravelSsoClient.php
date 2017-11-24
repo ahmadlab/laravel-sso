6a4b43d47e4223e0475c0231b8076d79062a60ea
ahmadnorin <ahmadnorin@gmailcom>
2017-11-24 21:15:35 +0700

    initial commit

---
+++ src/LaravelSsoClient.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @Author: ahmadnorin
+ * @Date:   2017-11-24 20:06:14
+ * @Last Modified by:   ahmadnorin
+ * @Last Modified time: 2017-11-24 21:14:26
+ */
+namespace Ahmadlab\LaravelSsoClient;
+
+class LaravelSsoClient
+{
+	static function index()
+	{
+		return 'package loaded';
+	}
+}
